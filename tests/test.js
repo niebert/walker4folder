ab2f7bb836fec6636c629b4bc1479e6bfe783650
Engelbert Niehaus <niehaus@uni-landau.de>
2020-11-22 17:12:34 +0100

    replace in file and create output added as test case

--- tests/test.js
+++ tests/test.js
@@ -16,9 +16,10 @@ function my_handle_dir(file,pathItem,dirAppend) {
 function replace4file(file,pathItem,dirAppend) {
   if (w4f.get_extension(file) == "html") {
     var vContent = w4f.load_file(pathItem);
-    vContent = vContent.replace("v0.7.1.min","v1.0.4-master");
+    vContent = vContent.replace("aframe-v0.7.1.min.js","aframe-v1.0.4.js");
     console.log("Replaced: "+vContent);
-    w4f.save_file(pathItem+".out",vContent);
+    out_filename = w4f.remove_extension(pathItem) + "_out.html";
+    w4f.save_file(out_filename,vContent);
   }
 }
 //w4f.walker4folder();
@@ -26,4 +27,5 @@ function replace4file(file,pathItem,dirAppend) {
 //w4f.walker4folder("./src",my_handle_file);
 //w4f.walker4folder("./src",my_handle_file,my_handle_dir);
 //w4f.walker4folder("./node_modules",my_handle_file,my_handle_dir);
+//w4f.walker4folder("../../WebVR/HuginSample/docs",replace4file,my_handle_dir);
 w4f.walker4folder("./tests",replace4file,my_handle_dir);
